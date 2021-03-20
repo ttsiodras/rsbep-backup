66e223c5ce6e3141457ed4e3ace8fe33b3290eef
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-03-20 15:42:24 +0100

    improve shell scripts
    - melt.sh:
      Read header, size and payload from a single data stream.
      This is much simpler and faster.
    
    - freeze.sh:
      - Use command group instead of subshell which is faster.
      - Remove '/bin' prefix from 'ls'.
        This prefix is also not used for other commands like 'awk'.
        The prefix made this script incompatible with systems that don't
        install coreutils to /bin/.

--- contrib/melt.sh
+++ contrib/melt.sh
@@ -4,10 +4,13 @@ if [ $# -ne 1 ] ; then
 	exit 1
 fi
 if [ ! -f "$1" ] ; then echo $1 is not a regular file\! ; exit 1 ; fi
-SIG=`dd if="$1" conv=noerror,notrunc 2>/dev/null | rsbep -d -B 255 -D 223 -R 4080 | head -1`
-if [ "$SIG" != "MAGIC_TTSIOD_RS" ] ; then
-	echo "Beyond capacity to correct... Sorry." > /dev/stderr
-	exit 1
-fi
-SIZE=`dd if="$1" conv=noerror,sync 2>/dev/null | rsbep -d -B 255 -D 223 -R 4080 | head -2 | tail -1`
-dd if="$1" conv=noerror,sync 2>/dev/null | rsbep -d -B 255 -D 223 -R 4080 | sed '1,2d' 2>/dev/null | rsbep_chopper $SIZE
+
+dd if="$1" conv=noerror,sync 2>/dev/null | rsbep -d -B 255 -D 223 -R 4080 | {
+	read SIG
+	if [ "$SIG" != "MAGIC_TTSIOD_RS" ] ; then
+		echo "Beyond capacity to correct... Sorry." > /dev/stderr
+		exit 1
+	fi
+	read SIZE
+	rsbep_chopper $SIZE
+}
