474da283383d826639be11cd6d75c5beee60cdf0
slowpeek <slowpeek@users.noreply.github.com>
2023-01-09 01:15:13 +0200

    Improve freeze.sh and melt.sh
    - unify the code
    - reformat for better readability

--- contrib/freeze.sh
+++ contrib/freeze.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
-if [ $# -ne 1 ] ; then
-	echo Usage: $0 filename \(... will output to stdout\)
-	exit 1
+
+MAGIC=MAGIC_TTSIOD_RS
+RSBEP='rsbep -B 255 -D 223 -R 4080'
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 filename (... will output to stdout)"
+    exit 1
 fi
-if [ ! -f "$1" ] ; then echo \"$1\" is not a regular file\! ; exit 1 ; fi
+
+if [ ! -f "$1" ]; then
+    echo "'$1' is not a regular file!"
+    exit 1
+fi
+
 # stat -c doesn't work under OS/X
 # SIZE=`stat -c '%s' "$1"`
-SIZE=`ls -l "$1" | awk '{print $5}'`
-{ echo MAGIC_TTSIOD_RS ; echo $SIZE ; cat "$1"; } | rsbep -B 255 -D 223 -R 4080
+SIZE=$(ls -l "$1" | awk '{print $5}')
+
+{
+    echo "$MAGIC"
+    echo "$SIZE"
+    cat "$1"
+} | $RSBEP
--- contrib/melt.sh
+++ contrib/melt.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
-if [ $# -ne 1 ] ; then
-	echo Usage: $0 filename   \(...will output to stdout\)
-	exit 1
-fi
-if [ ! -f "$1" ] ; then echo $1 is not a regular file\! ; exit 1 ; fi
 
-dd if="$1" conv=noerror,sync 2>/dev/null | rsbep -d -B 255 -D 223 -R 4080 | {
-	read SIG
-	if [ "$SIG" != "MAGIC_TTSIOD_RS" ] ; then
-		echo "Beyond capacity to correct... Sorry." > /dev/stderr
-		exit 1
-	fi
-	read SIZE
-	rsbep_chopper $SIZE
+MAGIC=MAGIC_TTSIOD_RS
+RSBEP='rsbep -B 255 -D 223 -R 4080'
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 filename (... will output to stdout)"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "'$1' is not a regular file!"
+    exit 1
+fi
+
+dd if="$1" conv=noerror,sync 2>/dev/null | $RSBEP -d | {
+    read -r SIG
+
+    if [ "$SIG" != "$MAGIC" ]; then
+        echo 'Beyond capacity to correct... Sorry.' >&2
+        exit 1
+    fi
+
+    read -r SIZE
+    rsbep_chopper "$SIZE"
 }
