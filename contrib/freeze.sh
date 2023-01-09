38cc33749f593dacf828ae941a50d725068283b2
slowpeek <slowpeek@users.noreply.github.com>
2023-01-10 01:47:28 +0200

    Allow pathes with leading dash in freeze.sh

--- contrib/freeze.sh
+++ contrib/freeze.sh
@@ -14,12 +14,12 @@ fsize() {
     # stat -c doesn't work under OS/X
     # SIZE=`stat -c '%s' "$1"`
 
-    set -- $(LC_ALL=C ls -ln "$1")
+    set -- $(LC_ALL=C ls -ln -- "$1")
     echo "$5"
 }
 
 {
     echo MAGIC_TTSIOD_RS
     fsize "$1"
-    cat "$1"
+    cat -- "$1"
 } | rsbep -B 255 -D 223 -R 4080
